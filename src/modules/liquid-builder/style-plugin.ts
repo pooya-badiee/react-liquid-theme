a6481a8f4fa660934a667640721f8f27f415dd19
pooya <pooya.badiee76@gmail.com>
2025-05-18 07:04:08 -0700

    add basic css/sass support

--- src/modules/liquid-builder/style-plugin.ts
+++ src/modules/liquid-builder/style-plugin.ts
@@ -1,28 +1,45 @@
 import type { InputPluginOption } from 'rollup'
 import { createFilter } from '@rollup/pluginutils'
-import sass from 'sass'
+import { compileString as complieSass } from 'sass'
+import postcss from 'postcss'
 
-export function createStylePlugin(): InputPluginOption {
+interface Options {
+  output?: string
+}
+
+export function createStylePlugin({ output = 'main.css' }: Options = {}): InputPluginOption {
   const cssFilter = createFilter(['**/*.css'])
   const scssFilter = createFilter(['**/*.scss'])
+  const processor = postcss([])
+  const collectedCss: string[] = []
+
   return {
     name: 'liquid-style',
-
-    transform(code, id) {
+    transform: async (code, id) => {
       const isCss = cssFilter(id)
       const isScss = scssFilter(id)
       if (!isCss && !isScss) return
       let compiledCode = code
       if (isScss) {
-        compiledCode = sass.compileString(code).css
+        compiledCode = complieSass(code).css
       }
-
+      const result = await processor.process(compiledCode)
+      collectedCss.push(result.css)
       return {
-        code: `export default ${JSON.stringify(compiledCode)}`,
+        code: `export default ${JSON.stringify(result.css)}`,
         moduleType: 'js',
         map: null,
         moduleSideEffects: false,
       }
     },
+    generateBundle() {
+      if (!collectedCss.length) return
+      const finalCss = collectedCss.join('\n')
+      this.emitFile({
+        type: 'asset',
+        fileName: output,
+        source: finalCss,
+      })
+    }
   }
 }
