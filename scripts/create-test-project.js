6950fddcd8c87d460b3d6c760c5ebc7d8dbd7353
pooya <pooya.badiee76@gmail.com>
2025-06-14 02:24:44 -0700

    add create-test-project script to automate test project setup

---
+++ scripts/create-test-project.js
@@ -0,0 +1,71 @@
+import * as path from 'node:path'
+import * as fs from 'node:fs'
+import { execSync } from 'node:child_process'
+
+const testProjectPath = path.resolve(process.cwd(), '..', 'react-liquid-theme-test')
+const testProjectPackageJsonPath = path.join(testProjectPath, 'package.json')
+// we first delete the test project if it exists
+fs.rmSync(testProjectPath, { recursive: true, force: true })
+// then we create the test project
+fs.mkdirSync(testProjectPath)
+// we create the package.json file
+const packageJson = {
+  name: 'react-liquid-theme-test',
+  version: '1.0.0',
+  private: true,
+  type: 'module',
+  dependencies: {
+    react: '^19',
+    'react-dom': '^19',
+    typescript: 'latest',
+    '@types/react': 'latest',
+    '@types/react-dom': 'latest',
+    'react-liquid-theme': 'file:../react-liquid-theme',
+  },
+  scripts: {
+    build: 'node ./node_modules/react-liquid-theme/dist/cli.js generate',
+    setup: 'node ./node_modules/react-liquid-theme/dist/cli.js setup',
+  },
+}
+// write the package.json file
+fs.writeFileSync(testProjectPackageJsonPath, JSON.stringify(packageJson, null, 2))
+// we now install
+execSync('npm install', {
+  cwd: testProjectPath,
+  stdio: 'inherit',
+})
+
+// now we create the src directory
+const srcPath = path.join(testProjectPath, 'src')
+fs.mkdirSync(srcPath)
+// now the global.d.ts file
+const GLOBAL_D_TS_CONTENT = `/// <reference types="react-liquid-theme/declarations" />`
+fs.writeFileSync(path.join(srcPath, 'global.d.ts'), GLOBAL_D_TS_CONTENT)
+// now we create the file.snippet.tsx files
+for (let index = 1; index <= 100; index++) {
+  const fileSnippetPath = path.join(srcPath, `file.${index}.snippet.tsx`)
+  const fileSnippetContent = `export default function File${index}Snippet() {
+    return <div>File ${index} Snippet</div>
+  }
+  `
+  fs.writeFileSync(fileSnippetPath, fileSnippetContent)
+}
+
+// now we create the tsconfig.json file
+const tsconfigPath = path.join(testProjectPath, 'tsconfig.json')
+fs.writeFileSync(
+  tsconfigPath,
+  JSON.stringify(
+    {
+      compilerOptions: {
+        jsx: 'preserve',
+        module: 'preserve',
+        moduleResolution: 'bundler',
+        skipLibCheck: true,
+        target: 'esnext',
+      },
+    },
+    null,
+    2
+  )
+)
